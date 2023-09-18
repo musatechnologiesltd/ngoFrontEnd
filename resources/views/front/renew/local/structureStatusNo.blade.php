1e82410a9231b544d607a963137f80c58ab8974f
Musa Technologies LTD <113629203+musatechnologiesltd@users.noreply.github.com>
2023-09-18 16:31:38 +0600

    update 18 september

--- resources/views/front/renew/local/structureStatusNo.blade.php
+++ resources/views/front/renew/local/structureStatusNo.blade.php
@@ -3,7 +3,7 @@
         <div class="mb-3">
             <label class="form-label" for="">
                 'অপরিবর্তিত' প্রশংসাপত্রের অনুলিপি (সংশ্লিষ্ট দেশের পিস অফ জাস্টিস ডিপার্টমেন্ট দ্বারা নোটারাইজড/প্রত্যয়িত) যদি সংস্থার গঠনতন্ত্র পরিবর্তিত না হয় : <span class="text-danger">*</span>
-            <br><span class="text-danger">পিডিএফ এর সাইজ ৫০০ কেবি বেশি হওয়া যাবে না</span>  </label>
+            <br><span class="text-success">পিডিএফ এর সাইজ ৫০০ কেবি বেশি হওয়া যাবে না</span>  </label>
             <input class="form-control" name="constitution_of_the_organization_if_unchanged" data-parsley-required accept=".pdf" type="file" id="">
         </div>
     </div>
